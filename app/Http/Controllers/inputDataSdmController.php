475fcfe6373c95c19864e2a068b04896de28a03d
kurob <kurob1993@gmail.com>
2018-10-11 15:28:38 +0700

    Penambahan Fungsi untuk menambahkan upload semua data sdm

--- app/Http/Controllers/inputDataSdmController.php
+++ app/Http/Controllers/inputDataSdmController.php
@@ -16,20 +16,25 @@ class inputDataSdmController extends Controller
     }
     public function index(Request $request)
     {
+        
         $data_group = $request->get('data_group');
         $data_menu  = $request->get('data_menu');
-        $data       = ['demografi'=>'Demografi','mhl'=>'Man Hour loss','kpi'=>'Pencapaian KPI'];
+        $data       = [ 
+                        'demografi' => 'Demografi',
+                        'mhl'       => 'Man Hour Loss',
+                        'kpi'       => 'Pencapaian KPI'
+                      ];
         return view('sdm.inputDataSdm',
             [
                 'data_group' =>$data_group,
                 'data_menu'  => $data_menu,
                 'data'  => $data,
+                'tahun_lama' => $tahun_lama
             ]
         );
     }
     public function upload(Request $request)
     {
-        
         if($request->data !== 'mhl'){
             $rules = [
                 'file' => 'required|file|max:1000|mimes:xlsx,XLSX', // ukuran dihitung dalam KB
@@ -45,7 +50,6 @@ class inputDataSdmController extends Controller
             ];
         }
         
-
         $customMessages = [
             'max' => 'Ukuran File Lebih dari :max KB',
             'required' => ':attribute Tidak Boleh Kosong.',
@@ -55,11 +59,36 @@ class inputDataSdmController extends Controller
         $this->validate($request, $rules, $customMessages);
 
         $uploadedFile = $request->file('file');        
-        $path = $uploadedFile->storeAs('public/files/sdm/'.date('Y').'/'.$request->status, date('Ym').'.xlsx' );
+        $path = $uploadedFile->storeAs('public/files/sdm/demografi/'.$request->tahun.'/'.$request->berdasarkan, date('Ym').'.xlsx' );
 
-        return redirect('/sdm/input_demografi/store/'.$request->tahun.'/'.$request->status);
+        return redirect('/sdm/input_data_sdm/store/'.$request->data.'/'.$request->tahun.'/'.$request->berdasarkan);
     }
 
+    public function store($data,$tahun,$status)
+    {
+        $inputFileName  = './public/storage/files/sdm/'.$data.'/'.$tahun.'/'.$status.'/'.date('Ym').'.xlsx';
+        $spreadsheet    = IOFactory::load($inputFileName);
+        $sheetData      = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
+        
+        switch ($status) {
+            case 'status':
+            case 'golongan':
+            case 'pendidikan':
+                $msg = $this->gps($sheetData,$tahun,$status);
+                break;
+
+            case 'usia':
+                $msg = $this->usia($sheetData,$tahun,$status);
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+        
+        return redirect()->back()->with('message', $msg);
+    }
+    
     // fungsi untuk input data demografi berdasarkan golongan/pendidikan/status
     public function gps($sheetData,$tahun,$status)
     {
@@ -73,9 +102,6 @@ class inputDataSdmController extends Controller
                     DB::table('demografi')->where('tahun',$tahun)->where('part',$status)->where('id',$value['A'])
                     ->update(
                         [ 
-                            // 'id'=>$value['A'],
-                            // 'tahun'=>$tahun,
-                            // 'part'=>$status,
                             'deskripsi'=>$value['B'],
                             'des_lama'=>$value['C'],
 
@@ -129,33 +155,7 @@ class inputDataSdmController extends Controller
         }
         return $msg;
     }
-    public function store($tahun,$status)
-    {
-        $inputFileName  = './public/storage/files/sdm/'.date('Y').'/'.$status.'/'.date('Ym').'.xlsx';
-        $spreadsheet    = IOFactory::load($inputFileName);
-        $sheetData      = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
-        
-        // fungsi untuk input data demografi berdasarkan golongan/pendidikan/status
-        
-
-        switch ($status) {
-            case 'status':
-            case 'golongan':
-            case 'pendidikan':
-                $msg = $this->gps($sheetData,$tahun,$status);
-                break;
-
-            case 'usia':
-                $msg = $this->gps($sheetData,$tahun,$status);
-                break;
-            
-            default:
-                # code...
-                break;
-        }
-        
-        return redirect()->back()->with('message', $msg);
-    }
+    
     public function berdasarkan($data = null)
     {
         $ret = '';
@@ -163,10 +163,10 @@ class inputDataSdmController extends Controller
             case 'demografi':
                     $ret = ['results'=>
                         [
-                            ['id'=>1,'text'=>'status'],
-                            ['id'=>2,'text'=>'golongan'],
-                            ['id'=>3,'text'=>'pendidikan'],
-                            ['id'=>4,'text'=>'usia'],
+                            ['id'=>'status','text'=>'Status'],
+                            ['id'=>'golongan','text'=>'Golongan'],
+                            ['id'=>'pendidikan','text'=>'Pendidikan'],
+                            ['id'=>'usia','text'=>'Usia'],
                         ]
                     ];
                 break;
@@ -174,10 +174,10 @@ class inputDataSdmController extends Controller
             case 'kpi':
                     $ret = ['results'=>
                         [
-                            ['id'=>1,'text'=>'PENCAPAIAN KPI PERUSAHAAN'],
-                            ['id'=>2,'text'=>'DIREKTORAT SDM & PU'],
-                            ['id'=>3,'text'=>'SUBDIT HUMAN CAPITAL MANAGEMENT'],
-                            ['id'=>4,'text'=>'DIVISI PERFORMANCE MGT & CORPORATE CULTURE'],
+                            ['id'=>'kpi','text'=>'PENCAPAIAN KPI PERUSAHAAN'],
+                            ['id'=>'sdm&pu','text'=>'DIREKTORAT SDM & PU'],
+                            ['id'=>'shcm','text'=>'SUBDIT HUMAN CAPITAL MANAGEMENT'],
+                            ['id'=>'dpm&c','text'=>'DIVISI PERFORMANCE MGT & CORPORATE CULTURE'],
                         ]
                     ];
                 break;
