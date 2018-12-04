d55af5c71a45023ad6cf63d1a88a08810ad6aa1d
curlykun <kurob1993@gmail.com>
2018-12-04 11:00:32 +0700

    controller input sdm

--- app/Http/Controllers/inputDataSdmController.php
+++ app/Http/Controllers/inputDataSdmController.php
@@ -34,6 +34,7 @@ class inputDataSdmController extends Controller
     }
     public function upload(Request $request)
     {
+
         if($request->data !== 'mhl'){
             $rules = [
                 'file' => 'required|file|max:1000|mimes:xlsx,XLSX', // ukuran dihitung dalam KB
@@ -48,7 +49,7 @@ class inputDataSdmController extends Controller
                 'tahun' => 'required',
             ];
         }
-        
+
         $customMessages = [
             'max' => 'Ukuran File Lebih dari :max KB',
             'required' => ':attribute Tidak Boleh Kosong.',
@@ -56,11 +57,11 @@ class inputDataSdmController extends Controller
         ];
 
         $this->validate($request, $rules, $customMessages);
-
+        
         $uploadedFile = $request->file('file');        
-        $path = $uploadedFile->storeAs('public/files/sdm/demografi/'.$request->tahun.'/'.$request->berdasarkan, date('Ym').'.xlsx' );
+        $path = $uploadedFile->storeAs('public/files/sdm/'.$request->data.'/'.$request->tahun.'/'.$request->berdasarkan, date('Ym').'.xlsx' );
 
-        return redirect('/sdm/input_data_sdm/store/'.$request->data.'/'.$request->tahun.'/'.$request->berdasarkan);
+        return $this->store($request->data, $request->tahun, $request->berdasarkan);
     }
 
     public function store($data,$tahun,$status)
@@ -69,6 +70,151 @@ class inputDataSdmController extends Controller
         $spreadsheet    = IOFactory::load($inputFileName);
         $sheetData      = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
         
+        switch ($data) {
+            case 'demografi':
+                $msg = $this->demografi($sheetData,$tahun,$status);
+                break;
+
+            case 'mhl':
+                $msg = $this->mhl($sheetData,$tahun,$status);
+                break;
+            
+            default:
+                # code...
+                break;
+        }
+        return redirect()->back()->with('message',$msg);
+    }
+
+    public function mhl($sheetData,$tahun,$status)
+    {
+        $msg = '';
+        foreach ($sheetData as $key => $value) {
+            if($key > 3){
+                $count = DB::table('mhl')->where('tahun',$tahun)->where('kode',$value['A'])->count();
+                if( $count == 0){
+                    DB::table('mhl')->insert(
+                        [ 
+                            'kode' => $value['A'],
+                            'tahun' => $tahun,
+                            'namaunit' => htmlentities($value['B']),
+                            'jan_hari_krj' => $value['C'],
+                            'jan_jml_kry' => $value['D'],
+                            'jan_lost_menit' => $value['E'],
+                            'jan_lost_hari' => $value['F'],
+                            'feb_hari_krj' => $value['G'],
+                            'feb_jml_kry' => $value['H'],
+                            'feb_lost_menit' => $value['I'],
+                            'feb_lost_hari' => $value['J'],
+                            'mar_hari_krj' => $value['K'],
+                            'mar_jml_kry' => $value['L'],
+                            'mar_lost_menit' => $value['M'],
+                            'mar_lost_hari' => $value['N'],
+                            'apr_hari_krj' => $value['O'],
+                            'apr_jml_kry' => $value['P'],
+                            'apr_lost_menit' => $value['Q'],
+                            'apr_lost_hari' => $value['R'],
+                            'mei_hari_krj' => $value['S'],
+                            'mei_jml_kry' => $value['T'],
+                            'mei_lost_menit' => $value['U'],
+                            'mei_lost_hari' => $value['V'],
+                            'jun_hari_krj' => $value['W'],
+                            'jun_jml_kry' => $value['X'],
+                            'jun_lost_menit' => $value['Y'],
+                            'jun_lost_hari' => $value['Z'],
+                            'jul_hari_krj' => $value['AA'],
+                            'jul_jml_kry' => $value['AB'],
+                            'jul_lost_menit' => $value['AC'],
+                            'jul_lost_hari' => $value['AD'],
+                            'agu_hari_krj' => $value['AE'],
+                            'agu_jml_kry' => $value['AF'],
+                            'agu_lost_menit' => $value['AG'],
+                            'agu_lost_hari' => $value['AH'],
+                            'sep_hari_krj' => $value['AI'],
+                            'sep_jml_kry' => $value['AJ'],
+                            'sep_lost_menit' => $value['AK'],
+                            'sep_lost_hari' => $value['AL'],
+                            'okt_hari_krj' => $value['AM'],
+                            'okt_jml_kry' => $value['AN'],
+                            'okt_lost_menit' => $value['AO'],
+                            'okt_lost_hari' => $value['AP'],
+                            'nov_hari_krj' => $value['AQ'],
+                            'nov_jml_kry' => $value['AR'],
+                            'nov_lost_menit' => $value['AS'],
+                            'nov_lost_hari' => $value['AT'],
+                            'des_hari_krj' => $value['AU'],
+                            'des_jml_kry' => $value['AV'],
+                            'des_lost_menit' => $value['AW'],
+                            'des_lost_hari' => $value['AX'],
+                            'unit_aktif' => $value['AY'],
+                        ]
+                    );
+                    $msg = "Data berhasil di tambahkan";
+                }else{
+                    DB::table('MHL')->where('tahun',$tahun)->where('kode',$value['A'])
+                    ->update(
+                        [ 
+                            'namaunit' => htmlentities($value['B']),
+                            'jan_hari_krj' => $value['C'],
+                            'jan_jml_kry' => $value['D'],
+                            'jan_lost_menit' => $value['E'],
+                            'jan_lost_hari' => $value['F'],
+                            'feb_hari_krj' => $value['G'],
+                            'feb_jml_kry' => $value['H'],
+                            'feb_lost_menit' => $value['I'],
+                            'feb_lost_hari' => $value['J'],
+                            'mar_hari_krj' => $value['K'],
+                            'mar_jml_kry' => $value['L'],
+                            'mar_lost_menit' => $value['M'],
+                            'mar_lost_hari' => $value['N'],
+                            'apr_hari_krj' => $value['O'],
+                            'apr_jml_kry' => $value['P'],
+                            'apr_lost_menit' => $value['Q'],
+                            'apr_lost_hari' => $value['R'],
+                            'mei_hari_krj' => $value['S'],
+                            'mei_jml_kry' => $value['T'],
+                            'mei_lost_menit' => $value['U'],
+                            'mei_lost_hari' => $value['V'],
+                            'jun_hari_krj' => $value['W'],
+                            'jun_jml_kry' => $value['X'],
+                            'jun_lost_menit' => $value['Y'],
+                            'jun_lost_hari' => $value['Z'],
+                            'jul_hari_krj' => $value['AA'],
+                            'jul_jml_kry' => $value['AB'],
+                            'jul_lost_menit' => $value['AC'],
+                            'jul_lost_hari' => $value['AD'],
+                            'agu_hari_krj' => $value['AE'],
+                            'agu_jml_kry' => $value['AF'],
+                            'agu_lost_menit' => $value['AG'],
+                            'agu_lost_hari' => $value['AH'],
+                            'sep_hari_krj' => $value['AI'],
+                            'sep_jml_kry' => $value['AJ'],
+                            'sep_lost_menit' => $value['AK'],
+                            'sep_lost_hari' => $value['AL'],
+                            'okt_hari_krj' => $value['AM'],
+                            'okt_jml_kry' => $value['AN'],
+                            'okt_lost_menit' => $value['AO'],
+                            'okt_lost_hari' => $value['AP'],
+                            'nov_hari_krj' => $value['AQ'],
+                            'nov_jml_kry' => $value['AR'],
+                            'nov_lost_menit' => $value['AS'],
+                            'nov_lost_hari' => $value['AT'],
+                            'des_hari_krj' => $value['AU'],
+                            'des_jml_kry' => $value['AV'],
+                            'des_lost_menit' => $value['AW'],
+                            'des_lost_hari' => $value['AX'],
+                            'unit_aktif' => $value['AY'],
+                        ]
+                    );
+                    $msg = "Data berhasil di update";
+                }
+                
+            }
+        }
+        return $msg;
+    }
+    public function demografi($sheetData,$tahun,$status)
+    {
         switch ($status) {
             case 'status':
             case 'golongan':
@@ -84,9 +230,7 @@ class inputDataSdmController extends Controller
                 # code...
                 break;
         }
-        return redirect()->back()->with('message',$msg);
     }
-
     // fungsi unttuk input data demografi berdasarkan usia
     public function demografiUsia($sheetData,$tahun,$status)
     {
