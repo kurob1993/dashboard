cf5e62e90208f6858615b62538bacdc4eed15101
curlykun <kurob1993@gmail.com>
2019-09-07 08:53:16 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // https
+        if(config('app.env') === 'production'){
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 
     /**
